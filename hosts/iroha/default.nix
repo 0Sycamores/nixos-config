253cbe8e2b8cf6efcefd9dee2e2df0b43ce223a1
Sycamore <0Sycamores@gamil.com>
2026-01-11 22:09:06 +0800

    feat(iroha): 调整内核视频分辨率为 1400x1200

--- hosts/iroha/default.nix
+++ hosts/iroha/default.nix
@@ -65,7 +65,7 @@
   virtualisation.vmware.guest.enable = true;
 
   # 强制指定一个高分辨率，例如 1920x1080 或 2560x1440 "Virtual-1" 是显示器名称
-  boot.kernelParams = [ "video=Virtual-1:1080x1080" ];
+  boot.kernelParams = [ "video=Virtual-1:1400x1200" ];
 
   # 防火墙配置
   # OpenSSH (22) 默认会打开防火墙，但在此处显式声明以便管理
